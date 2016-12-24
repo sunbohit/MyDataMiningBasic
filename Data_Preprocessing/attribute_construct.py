685f1fadab3758c5b342055b2aaa8cd6eed26bb7
sunbohit <a824834@163.com>
2016-12-24 16:15:24 +0800

    更新属性构造

--- Data_Preprocessing/attribute_construct.py
+++ Data_Preprocessing/attribute_construct.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 input_file = 'electricity_data.xls'
-ed_data = pd.read(input_file)
+output_file = 'electricity_data_after.xls'
+ed_data = pd.read_excel(input_file)
 
 ed_data['线损率'] = (ed_data['供入电量']-ed_data['供出电量'])/ed_data['供入电量']
+
+ed_data.to_excel(output_file)
