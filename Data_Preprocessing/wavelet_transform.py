1a10c3936431efa63e8e6f8f07c96949f5f2d064
sunbohit <a824834@163.com>
2016-12-25 19:34:14 +0800

    更新小波变换

--- Data_Preprocessing/wavelet_transform.py
+++ Data_Preprocessing/wavelet_transform.py
@@ -1,1 +1,11 @@
 from scipy.io import loadmat
+
+inputfile = 'leleccum.mat'
+mat = loadmat(inputfile)
+print(mat)
+'''
+{'leleccum': array([[ 420.20278994,  423.52653517,  423.52271225, ...,  323.96580997,
+         323.2400761 ,  323.85476049]])}
+
+'''
+signal = mat['leleccum'][0]
