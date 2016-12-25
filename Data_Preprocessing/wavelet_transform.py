e4a8aa28b6140dc71240a27d4ce100d235753f8d
sunbohit <a824834@163.com>
2016-12-25 19:42:13 +0800

    更新小波变换

--- Data_Preprocessing/wavelet_transform.py
+++ Data_Preprocessing/wavelet_transform.py
@@ -1,4 +1,5 @@
 from scipy.io import loadmat
+import pywt #sudo pip3 install PyWavelets
 
 inputfile = 'leleccum.mat'
 mat = loadmat(inputfile)
@@ -9,3 +10,10 @@ print(mat)
 
 '''
 signal = mat['leleccum'][0]
+print(signal)
+'''
+[ 420.20278994  423.52653517  423.52271225 ...,  323.96580997  323.2400761
+  323.85476049]
+'''
+
+c = pywt.wavedec(signal, 'bior3.7', level=5)
