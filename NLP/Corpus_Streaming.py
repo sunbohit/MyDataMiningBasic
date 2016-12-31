5810f1bd2764c91b5c44a5d3218d4e32cd17f16f
sunbohit <a824834@163.com>
2016-12-31 14:32:04 +0800

    添加文本从硬盘读取节省内存

---
+++ NLP/Corpus_Streaming.py
@@ -0,0 +1,30 @@
+from gensim import corpora
+
+dictionary = corpora.Dictionary()
+dictionary = dictionary.load('deerwester.dict')
+#print(dictionary.token2id)
+'''
+{'computer': 0, 'trees': 9, 'interface': 2, 'system': 6, 'survey': 5, 'graph': 10, 'eps': 8, 'human': 1, 'user': 3, 'minors': 11, 'time': 4, 'response': 7}
+'''
+class MyCorpus(object):
+	def __iter__(self):
+		for line in open('mycorpus.txt'):
+			# assume there's one document per line, tokens separated by whitespace
+			yield dictionary.doc2bow(line.lower().split())
+
+corpus_memory_friendly = MyCorpus()  # doesn't load the corpus into memory!
+#print(corpus_memory_friendly) #<__main__.MyCorpus object at 0x7fa9e58a7780>
+
+for vector in corpus_memory_friendly:  # load one vector into memory at a time
+	print(vector)
+'''
+[(0, 1), (1, 1), (2, 1)]
+[(0, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+[(2, 1), (3, 1), (6, 1), (8, 1)]
+[(1, 1), (6, 2), (8, 1)]
+[(3, 1), (4, 1), (7, 1)]
+[(9, 1)]
+[(9, 1), (10, 1)]
+[(9, 1), (10, 1), (11, 1)]
+[(5, 1), (10, 1), (11, 1)]
+'''
