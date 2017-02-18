1cc374f77cf7c21a61d017c3e74e9e90fc3b8b7c
sunbohit <a824834@163.com>
2017-02-18 22:00:18 +0800

    更新数据探索

--- Data_Discovery/outlier_detection.py
+++ Data_Discovery/outlier_detection.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 #plt.rcParams['font.sans-serif'] = ['SimHei']
 #plt.rcParams['axes.unicode_minus'] = False
 
-cs_path = 'catering_sale.xls'
-cs_data = pd.read_excel(cs_path,index_col='日期')
-#print(cs_data)
+cs_path = 'catering_sale.xls' # 数据集路径
+cs_data = pd.read_excel(cs_path,index_col='日期') # 用pandas读入excel数据，将日期作为索引
+print(cs_data)
 print(type(cs_data))
+
 plt.figure()
 p = cs_data.boxplot()
 
